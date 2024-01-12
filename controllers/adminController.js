fc59d745d012bd6ca53897b8423220db3d49c209
Mubina <mubina3120@gmail.com>
2024-01-12 21:04:35 +0900

    feat: Develop logout logic and modify login process

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -14,14 +14,17 @@ adminController.loginProcess = async (req, res) => {
         const result = await member.loginData(data);
 
         // SESSION 
-        req.session.member = result; 
-        
-        req.session.save(function() { 
-            result.mb_type === 'ADMIN' 
-                ? res.redirect("/admin/home") 
-                : res.redirect("/admin");
-        });
-
+        if (result.mb_type === 'ADMIN') {
+            req.session.member = result; 
+            req.session.save(function() {
+                res.redirect("/admin");
+            })
+        } else {
+                res.send(`
+                    <script>alert("Admin Homepagega faqatgina admin kira oladi!"); 
+                    window.location.replace('/admin'); </script>
+                `);
+        }
     } catch(err){
         console.log(`ERROR: Login Process! ${err.message}`);
 
@@ -33,23 +36,7 @@ adminController.loginProcess = async (req, res) => {
     }
 };
 
-adminController.login = async (req, res) => {
-    try{
-        console.log("GET: Admin HomePage");
-
-        res.render("login");
-    } catch(err) {
-        console.log(`ERROR: ADMIN HomePage! ${err.message}`);
-        
-        const error = {
-            state: "Fail",
-            message: "Login qilishda xatolik bo'ldi!"
-        }
-        res.render("error", { error: error });
-    }
-};
-
-adminController.home  = async (req, res) => {
+adminController.home = async (req, res) => {
     try{
         console.log("GET: Admin HomePage");
 
@@ -59,7 +46,7 @@ adminController.home  = async (req, res) => {
         
         const error = {
             state: "Fail",
-            message: "HomePagega kirishda xatolik bo'ldi!"
+            message: "Login qilishda xatolik bo'ldi!"
         }
         res.render("error", { error: error });
     }
@@ -96,4 +83,18 @@ adminController.validateAdmin = (req, res, next) => {
                     
 		res.end(html);
 	}
+};
+
+adminController.logout = (req, res) => {
+    try{
+        console.log("GET: kimdir logout qilmoqda");
+
+        req.session.destroy(function() {
+            res.redirect("/admin");
+        });
+
+    } catch(err) {
+        console.log(`ERROR: logout qilishda xatolik boldi! ${err.message}`);
+        res.json({state: 'muvaffaqiyatsiz!', message: err.message});
+    }
 };
--- router_bssr.js
+++ router_bssr.js
@@ -3,17 +3,15 @@ const router_bssr = Router();
 
 const adminController = require("./controllers/adminController");
 
-router_bssr.get('/', adminController.login);
+router_bssr.get('/', adminController.home);
 router_bssr.post('/login', adminController.loginProcess);
-router_bssr.get(
-    '/home', 
-    adminController.validateAdmin,
-    adminController.home
-);
+
 router_bssr.get(
     '/adminpage', 
     adminController.validateAdmin,
     adminController.getAllMembers
 );
+
+router_bssr.get("/logout", adminController.logout);
 
 module.exports = router_bssr;
