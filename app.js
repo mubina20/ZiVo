c7fd90eef509e48ddfa2da2cd940bcbd2e49e4a4
Mubina <mubina3120@gmail.com>
2024-01-14 16:26:25 +0900

    fix: Modify app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const router_bssr = require('./router_bssr'); // BSSR
 const router = require('./router'); // SPA
 
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 // MongoDB connect
 let session = require('express-session');  
 const MongoDBStore = require('connect-mongodb-session')(session); 
@@ -15,8 +18,17 @@ const store = new MongoDBStore({
 
 // 1: Entry code 
 app.use(express.static('public')); 
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
+
+app.use(
+    cors({
+        credentials: true,
+        origin: true
+    })
+);
+app.use(cookieParser());
 
 // 2: Session code  
 app.use( 
