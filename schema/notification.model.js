5430586b8e1e8706af8bc81acfeca34fef44ca72
Mubina <mubina3120@gmail.com>
2024-01-25 22:03:33 +0900

    feat: Develop Notification Schema model

---
+++ schema/notification.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const { notification_enum_list } = require('../lib/config');
+
+const notificationSchema = new mongoose.Schema({
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Member', 
+        required: true,
+    },
+    recipientId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Member',
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: {
+            value: notification_enum_list,
+            message: "{VALUES} is not among permitted values!"
+        }        
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    isRead: {
+        type: Boolean,
+        default: false,
+    },
+}, { timestamps: true });
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+module.exports = Notification;
