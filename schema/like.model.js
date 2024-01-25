d22f7631a49c08f518281bf2feb9d3dc3571683f
Mubina <mubina3120@gmail.com>
2024-01-25 21:44:13 +0900

    feat: Develop Like schema model

---
+++ schema/like.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const { 
+    like_view_group_list, 
+    post_id_enum_list
+} = require("../lib/config");
+
+const likeSchema = new mongoose.Schema(
+    {
+        mb_id: { 
+            type: Schema.Types.ObjectId, 
+            ref: "Member",
+            required: true 
+        },
+        like_ref_id: {
+            type: Schema.Types.ObjectId,
+            required: true,
+        },
+        like_group: {
+            type: String,
+            required: true,
+            enum: {
+                values: like_view_group_list,
+            },
+        },
+        post_id: {
+            type: String,
+            required: false,
+            enum: {
+                values: post_id_enum_list,
+            }
+        }
+    },
+    { timestamps: { createdAt: true } }
+);
+
+module.exports = mongoose.model("Like", likeSchema);
