9a84e9f893541cf0b731beae538907b99587ab74
Mubina <mubina3120@gmail.com>
2024-02-10 10:50:11 +0900

    feat: Develop member page view

--- public/js/membersPage.js
+++ public/js/membersPage.js
@@ -21,3 +21,76 @@ $(function(){
     // Call
     countUp($numberElem, 0, $members.text(), 200);  
 });
+
+
+new Chart(document.getElementById("doughnut-chart"), {
+    type: 'doughnut',
+    data: {
+      labels: ["Africa", "Asia", "Europe"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,1267,734,784,433]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Zivo's members"
+      }
+    }
+});
+
+document.getElementById('charts-section').addEventListener('click', function(event) {
+  event.preventDefault(); 
+
+  const charts = document.querySelector('.charts');
+  const comments = document.querySelector('.comments');
+  const members = document.querySelector('.members');
+  
+  charts.classList.add('active');
+  comments.classList.remove('active');
+  members.classList.remove('active');
+});
+document.getElementById('comments-section').addEventListener('click', function(event) {
+  event.preventDefault(); 
+
+  const charts = document.querySelector('.charts');
+  const comments = document.querySelector('.comments');
+  const members = document.querySelector('.members');
+  
+  charts.classList.remove('active');
+  comments.classList.add('active');
+  members.classList.remove('active');
+});
+document.getElementById('members-section').addEventListener('click', function(event) {
+  event.preventDefault(); 
+
+  const charts = document.querySelector('.charts');
+  const comments = document.querySelector('.comments');
+  const members = document.querySelector('.members');
+  
+  charts.classList.remove('active');
+  comments.classList.remove('active');
+  members.classList.add('active');
+});
+
+
+
+function selectSection(section) {
+  // Barcha bo'limlarni tanlangan qilib olish
+  const sections = document.querySelectorAll('.section');
+
+  // Har bir bo'limdan 'active' klassini olib tashlash
+  sections.forEach(section => {
+      section.classList.remove('select_active');
+  });
+
+  // Tanlangan bo'limga 'active' klassini qo'shish
+  const selectedSection = document.querySelector('.' + section);
+  selectedSection.classList.add('select_active');
+}
+
+
