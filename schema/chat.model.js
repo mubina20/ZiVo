e5d371c59ea2125de48b36f1e5bf208ae039fd2e
Mubina <mubina3120@gmail.com>
2024-05-07 09:32:54 +0900

    fix: Fixed some pages design

--- schema/chat.model.js
+++ schema/chat.model.js
@@ -6,17 +6,6 @@ const ChatSchema = new mongoose.Schema({
         ref: 'Member',
         required: true
     }]
-    
-    // sender_id: {
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: "Member",
-    //     required: true
-    // },
-    // receiver_id: {
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: "Member",
-    //     required: true
-    // }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Chat', ChatSchema);
